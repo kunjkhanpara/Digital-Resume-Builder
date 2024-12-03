3979abc954d72dab5d84d52c74807ec9663aed40
kunjkhanpara <kunjpatel4647@gmail.com>
2024-12-03 19:14:18 +0530

    Add GitHub Pages deployment configuration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';  // Change here
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import AboutUs from './pages/AboutUs';
